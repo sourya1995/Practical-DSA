637a432fdd9509c20da069bf64be9337a86faf6e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-28 07:51:56 +0530

    Find Median Age

---
+++ Netflix/FindMedianFromADataStream.java
@@ -0,0 +1,32 @@
+import java.util.PriorityQueue;
+
+public class FindMedianFromADataStream {
+    PriorityQueue<Integer> maxHeapForSmallest;
+    PriorityQueue<Integer> minHeapForLargest;
+
+    public FindMedianFromADataStream(){
+        maxHeapForSmallest = new PriorityQueue<>((a, b) -> b -a);
+        minHeapForLargest = new PriorityQueue<>((a, b) -> a -b );
+    }
+
+    public void addNum(int num){
+        if(maxHeapForSmallest.isEmpty() || maxHeapForSmallest.peek() >= num)
+            maxHeapForSmallest.add(num);
+        else
+            minHeapForLargest.add(num);
+
+        if(maxHeapForSmallest.size() > minHeapForLargest.size() + 1)
+            minHeapForLargest.add(maxHeapForSmallest.poll());
+        else if(maxHeapForSmallest.size() < minHeapForLargest.size())
+            maxHeapForSmallest.add(minHeapForLargest.poll());
+    }
+
+    public double findMedian(){
+        if(maxHeapForSmallest.size() == minHeapForLargest.size()){
+            return maxHeapForSmallest.peek() / 2.0 + minHeapForLargest.peek() / 2.0;
+
+        }
+
+        return maxHeapForSmallest.peek();
+    }
+}
