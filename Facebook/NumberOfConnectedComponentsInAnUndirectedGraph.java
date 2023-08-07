6d464594e807ed4bd3779b9e35ab726fa2480ca4
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-07 09:43:09 +0530

    Number of Connected Components in an undirected graph

---
+++ Facebook/NumberOfConnectedComponentsInAnUndirectedGraph.java
@@ -0,0 +1,42 @@
+package Facebook;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class NumberOfConnectedComponentsInAnUndirectedGraph {
+    public int countComponents(int n, int[][] edges){
+        if (n <= 1) return n;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(i, new ArrayList<>());
+        }
+
+        for (int[] edge: edges) {
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(visited.add(i)){
+                dfs(i, map, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int n, Map<Integer, List<Integer>> map, Set<Integer> visited){
+        for(int j : map.get(i)){
+            if(visited.add(j)){
+                dfs(j, map, visited);
+            }
+        }
+    }
+}
